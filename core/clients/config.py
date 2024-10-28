6af4258d16222a54d48a6d0d606941fa85466d16
Tera Earlywine <dev@teraearlywine.com>
2024-10-28 15:47:16 -0700

    *

--- core/clients/config.py
+++ core/clients/config.py
@@ -18,10 +18,7 @@ class config:
         self.env = env
 
         # Keys / Tokens
-        self.github_token = self.__get_github_token()
-        self.gmail_app_username = self.__get_gmail_app_username()
-        self.gmail_app_password = self.__get_gmail_app_password()
-        
+   
         self.organization_id = self.__get_open_ai_organization_id()
         self.project_id = self.__get_open_ai_project_id()
         self.api_key = self.__get_open_ai_api_key()
@@ -32,26 +29,6 @@ class config:
             project=self.project_id,
             api_key=self.api_key,
         )
-
-    def __get_github_token(self):
-        """
-        Get the github token
-        """
-        return os.getenv("github_token")
-    
-    def __get_gmail_app_username(self):
-        """
-        Email username corresponds to the full email/gmail account
-        """
-        return os.getenv("gmail_app_username")
-
-    def __get_gmail_app_password(self):
-        """
-        Gets the user defined gmail app specific password 
-        api key
-        """
-
-        return os.getenv("gmail_app_password")
 
     def __get_open_ai_organization_id(self):
         return os.getenv("open_ai_organization_id")
