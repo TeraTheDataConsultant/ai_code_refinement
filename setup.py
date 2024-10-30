f3287ccb6668deec2724e84ebe1c82ced249050d
Tera Earlywine <dev@teraearlywine.com>
2024-10-29 22:16:52 -0700

    *

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read_requirements():
 
 setup(
     name="ai_code_refinement",
-    version="0.0.5",
+    version="0.0.6",
     description="AI Code Refinement",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
