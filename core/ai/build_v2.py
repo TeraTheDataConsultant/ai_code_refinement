ed826da89cb25e330c16d44ea68e4ad21c878435
Tera Earlywine <dev@teraearlywine.com>
2024-10-29 21:41:04 -0700

    *

--- core/ai/build_v2.py
+++ core/ai/build_v2.py
@@ -5,7 +5,30 @@ from core.files.file_management import FileManagement
 
 
 class Build: 
+    """
+    Build
+    -----
 
+    The build class automatically generates a new assistant + vector store 
+    in a OpenAI environment, configured for staging / production.
+
+    Usage::
+        env = 'staging'
+        build = Build(
+            assistant_name='format_text_assistant', 
+            assistant_description='Process uploaded data that was captured via an audio transcript.',
+            assistant_instructions=(
+                'fix any grammar related issues'
+                'streamline the text, look for patterns and summarize'
+                'if a user requests a templatized output, reference uploaded files prefixed template_'
+            ), 
+            dir=dir, 
+            env=env
+        )
+        build.new_assistant_resource()
+
+    ---
+    """
     def __init__(self, assistant_name, assistant_description, assistant_instructions, dir, env):
         self.assistant_name = f"assistant_resource_{assistant_name}"
         self.vector_name = f"vector_store_{self.assistant_name}"
@@ -22,7 +45,17 @@ class Build:
         self.client = self.cfg.openai_client
 
     def create_assistant(self):
-        # 1. Creates a new assistant
+        """
+        Create Assistant
+        ----------------
+        Create a new assistant
+
+        Step 1:
+        Using class attributes, creates a new assistant with pre-configured file search and 
+        code interpreter. 
+        
+        Assigns the newly created assistant_id to a variable
+        """
 
         new_assistant = self.client.beta.assistants.create(
             name=f'assistant_resource_{self.assistant_name}',
@@ -41,7 +74,14 @@ class Build:
         logger.info(f"New assistant created named assistant_resource_{self.assistant_name} & {self.assistant_id}")
 
     def create_vector(self): 
-        # 2. Creates a new vector object
+        """
+        Create Vector
+
+        Step 2: 
+        Create a vector object and add the assistant name as a suffix
+        This enables assistant <> vector attribution, since only one assistant can 
+        have one vector at a time.
+        """
 
         new_vector = self.client.beta.vector_stores.create(
             name=f'vector_store_{self.assistant_name}' 
@@ -50,9 +90,20 @@ class Build:
         logger.info(f"New vector created named vector_store_{self.assistant_name} & {self.vector_id}")
 
     def upload_files(self, dir, vector_id): 
+        """
+        Upload Files
+        ------------
+
+        Calls the FileManagement class, recursively uploads files 
+        for the specific directory to the specified vector id. 
+
+        TBD on this if it should be here. 
+        The vector_id should eventually be automatically attributed, 
+
+        """
         # 3. Uploads files to general storage
         
-        fm = FileManagement(env='staging', root_directory=dir)
+        fm = FileManagement(env=self.env, root_directory=dir)
         fm.upload_files_io(in_memory=True)
         fm.get_uploaded_files()
 
@@ -70,17 +121,11 @@ class Build:
         Tying it all together the new_assistant_resource method 
         first checks if either assistant_id or vector_id has a string assignment
         """
-        
+    
         # TODO: Refactor this so that it first checks to see if an assistant and vector 
         # exist. If they do, just pass the assistant_id and vector_id to the file upload 
         
-        # assistants = self.client.beta.assistants.list()
-        # for a in assistants.data:
-        #     if a.name == self.assistant_name:
-        #         self.assistant_id = a.id
-        #         logger.info(f"assistant exists: {self.assistant_name} * {self.assistant_id}")
-
-        if self.assistant_id is None and self.vector_id is None:             
+        if self.assistant_id is None:             
             self.create_assistant()
             self.create_vector()
 
@@ -92,35 +137,32 @@ class Build:
             }
         )
 
-        # Updates vector to reference any newly uploaded files 
-        # if self.file_ids is None: 
-        #     self.upload_files(dir=self.dir, vector_id=self.vector_id)
-
 
 if __name__ == "__main__": 
 
+    Build()
     # Pass environment and user input data to build 
     
-    env = 'staging'
+    # env = 'staging'
 
-    build = Build(
-        assistant_name='format_text_assistant', 
-        assistant_description='Process uploaded data that was captured via an audio transcript.',
-        assistant_instructions=(
-            'fix any grammar related issues'
-            'streamline the text, look for patterns and summarize'
-            'if a user requests a templatized output, reference uploaded files prefixed template_'
-        ), 
-        dir=dir, 
-        env=env
-    )
+    # build = Build(
+    #     assistant_name='format_text_assistant', 
+    #     assistant_description='Process uploaded data that was captured via an audio transcript.',
+    #     assistant_instructions=(
+    #         'fix any grammar related issues'
+    #         'streamline the text, look for patterns and summarize'
+    #         'if a user requests a templatized output, reference uploaded files prefixed template_'
+    #     ), 
+    #     dir=dir, 
+    #     env=env
+    # )
 
     # build.new_assistant_resource()
 
     # dir = '/Users/teraearlywine/Engineering/AI/audio/mnt/transcripts'
     # dir = '/Users/teraearlywine/Engineering/AI/audio/mnt/templates'
-    vector_id = build.vector_id
+    # vector_id = build.vector_id
 
     # Upload transcript files to assistant
-    if build.file_ids is None: 
-        build.upload_files(dir=dir, vector_id="vs_d18uzfWihHpwjsjKzTAerZTa")
+    # if build.file_ids is None: 
+    #     build.upload_files(dir=dir, vector_id="vs_d18uzfWihHpwjsjKzTAerZTa")
